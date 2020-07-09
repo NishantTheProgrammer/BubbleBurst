1ebf1be0154367d9838b7d885821e151590a4cbf
NishantTheProgrammer <knishant113@gmail.com>
2020-07-09 21:44:46 +0530

    Added checkCollusion and bubbleReachTop function

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 const fireSound = new Audio('./sounds/fire.mp3');
+const burstSound = new Audio('./sounds/burst.mp3');
+const powerDown = new Audio('./sounds/powerDown.mp3');
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 
 const app = new Vue({
     el: '.gameDiv',
     data(){
       return {
+          score: 0,
           x: 200,
           y: 200,
           angle: 0,
-          bullets: []
+          bullets: [],
+          bubbles: [],
+          bubbleReached: false
       }  
     },
     created() {
-        /*
-        --------------------------------This function will run for each bullet-------------------------------------
+       
+        this.updateBullet();
+        this.updateBubble();
         
-            1.  If bullet goes outside the Playground it will remove that bullet
-            2.  Update Bullet position
-        */
-        
-        setInterval(()=>{
-            const distance = 30;
-            this.bullets.forEach((bullet, i) => {
-                this.outOfScreen(i);
-                bullet.x += -Math.sin((Math.PI / 180) * (bullet.angle + 45)) * distance;
-                bullet.y += Math.cos((Math.PI / 180) * (bullet.angle + 45)) * distance;
-            });
-        }, 30)
     },
     methods: {
 
@@ -93,14 +91,85 @@ const app = new Vue({
         // It removes the bullets which are outside the playground
         outOfScreen(i)
         {
-            
-            if (this.bullets[i].x < 0  ||
-                this.bullets[i].y < 0 ||
-                this.bullets[i].x > 1000 ||
+            if (this.bullets[i].x < 0       ||
+                this.bullets[i].y < 0       ||
+                this.bullets[i].x > 1000    ||
                 this.bullets[i].y > 600)        
             {
                 this.bullets.splice(i, 1);
             }
+        },
+
+        updateBullet()
+        {
+            /*
+            --------------------------------This function will run for each bullet-------------------------------------
+            
+                1.  If bullet goes outside the Playground it will remove that bullet
+                2.  Update Bullet position
+            */
+            setInterval(()=>{
+                const distance = 30;
+                this.bullets.forEach((bullet, i) => {
+                    this.outOfScreen(i);
+                    bullet.x += -Math.sin((Math.PI / 180) * (bullet.angle + 45)) * distance;
+                    bullet.y += Math.cos((Math.PI / 180) * (bullet.angle + 45)) * distance;
+                });
+                this.checkCollusion();
+            }, 30)
+        }, 
+
+
+        // checking if any bullet is overlaping any bubble
+        checkCollusion()
+        {
+            this.bullets.forEach((bullet)=>{
+                this.bubbles.forEach((bubble, i)=>{
+                    if(
+                        Math.abs(bullet.x - bubble.x) < (25 + 7.5 ) &&       // 25 is radious of bubble
+                        Math.abs(bullet.y - bubble.y) < (25 + 7.5)           // 7.5 is radious of bullet
+                    ){
+                        this.bubbles.splice(i, 1);
+                        this.score++;
+                        burstSound.play();
+                    }
+                })
+            });
+        },
+
+        // When a bubble reached at the top of gameDiv
+        bubbleReachTop()
+        {
+            this.bubbles.forEach((bubble, i)=>{
+                if(bubble.y < 0)
+                {
+                    console.log('ho gya');
+                    this.bubbles.splice(i, 1);
+                    this.bubbleReached = true;
+                    powerDown.play();
+                    setTimeout(()=>{
+                        this.bubbleReached = false;
+                    }, 1000);
+                }
+            });
+        },
+
+        updateBubble()
+        {
+            const n = 50 / 2                // Height of bubble
+            setInterval(()=>{
+                this.bubbles.forEach(bubble => {
+                    bubble.y -= 2;
+                });
+                this.bubbleReachTop();
+            }, 30)
+            setInterval(()=>{               // Spawn bubble every 2 seconds
+                this.bubbles.push({
+                    x: random(n, 1000 - n),
+                    y: 600 + n,
+                    color: random(1, 10)
+                })
+            }, 2000)
         }
         
     },
@@ -119,5 +188,7 @@ const app = new Vue({
 })
 
 // listening to user input
+
 document.addEventListener("keydown", app.move);
 document.addEventListener("wheel", app.rotate);
+
